7960016e4dcb0397954c8ccf1768b0c2a899de6f
Rafael Breno <rafael_breno@outlook.com>
2020-09-07 20:06:05 -0300

    Create mini-micro-nano prototype of how to split the rules between rule and expected value

--- cmd/validator/validator.go
+++ cmd/validator/validator.go
@@ -2,6 +2,7 @@ package validator
 
 import (
 	"log"
+	"strings"
 	"reflect"
 )
 
@@ -10,7 +11,23 @@ func Validate(v reflect.Value) {
 
 	for i := 0; i < v.NumField(); i++ {
 		field := typeValid.Field(i)
-		log.Println("Field:", field.Name, " - Value:", v.Field(i))
-		log.Println("-_-_-_-_-_-_-_-_-_-_-_-")
+		// log.Println(field.Tag)
+		
+		for ruleKey, ruleValue := range filterRules(string(field.Tag)) {
+			log.Println(ruleKey, ruleValue)
+		}
+		// log.Println("Field:", field.Name, " - Value:", v.Field(i))
+		// log.Println("-_-_-_-_-_-_-_-_-_-_-_-")
 	}
+}
+
+func filterRules(rawRules string) map[string]string {
+	rules := strings.Split(strings.Replace(rawRules, "\"", "", -1), " ")
+	mapRules := make(map[string]string, 12)
+	for _, rule := range rules {
+		keyValue := strings.Split(rule, ":")
+		mapRules[string(keyValue[0])] = string(keyValue[1])
+	}
+
+	return mapRules
 }
